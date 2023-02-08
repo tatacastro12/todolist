6c658ae3f1258c16b8d7001ce62409d855f2e390
tatacastro12 <91699554+tatacastro12@users.noreply.github.com>
2023-02-08 09:42:50 -0800

    create components section and App

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,24 +1,40 @@
 import React from "react";
-import  react , { useState }  from "react" ;
+import react, { useState } from "react";
 
 const Todo = () => {
-    const [ isEditing , setIsEditing ] = useState ( false ) ;
-    const handledivdubleclick = ( ) => {setIsEditing ( true ) ;
-} ;
+    const [isEditing, setIsEditing] = useState(false);
+    const handledivdubleclick = () => {
+        setIsEditing(true);
+    };
+
+    const handleInputKeyDown = (e) => {
+        const key = e.keycode;
+        if (key === 13 || key === 27) {
+            setIsEditing(false);
+        }
+    };
 
     return (
-        isEditing?
-        <input/> :
-            <div className="row" ondoubleclick = { handledivdubleclick }>
+        isEditing ?
+            <div className="row" onDoubleClick={handledivdubleclick}>
+                <div className="column seven wide">
+                    <div className="ui input fluid">
+                        <input onKeyDown={handleInputKeyDown} />
+                    </div>
+                </div>
+            </div> :
+            <div className="row" onDoubleClick={handledivdubleclick}>
                 <div className="column five wide">
                     <h1>Test</h1>
                 </div>
-            <div className="column two wide">
-                <button className= "ui button circular icon green"><i className="white check icon"></i></button>
-            </div>
-            <div className="column two wide">
-                <button className= "ui button circular icon red"><i className= "white remove icon"></i></button>
-            </div>
+                <div className="column two wide">
+                    <button className="ui button circular icon green"><i className="white check icon"></i>
+                    </button>
+                </div>
+                <div className="column two wide">
+                    <button className="ui button circular icon red"><i className="white remove icon"></i>
+                    </button>
+                </div>
             </div>
     );
 };
