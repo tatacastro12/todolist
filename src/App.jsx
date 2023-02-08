830e5d626e6f644072a13f7843be213349d3ed27
tatacastro12 <91699554+tatacastro12@users.noreply.github.com>
2023-02-08 07:47:55 -0800

    create components section and App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
-function App() {
-  
-  return( "hola" )
+import React from 'react'
+import Form  from "./components/Form";
+ import Section from "./components/Section";
+
+
+const Apptitle = "to-do-App";
+
+const App = () => {
+  return (
+    <div className='ui container center aligned'>
+    <></>
+      <section>
+        <h1>
+          {Apptitle}
+        </h1>
+      </section>
+      <section>
+         <Form  /> 
+        <Section />
+      </section>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Section.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Section = ({ children }) => {
+  return (
+    <div style={{ margin: "50px" }}>
+      {children}
+    </div>
+  );
+};
+
+export default Section
+
+
+
