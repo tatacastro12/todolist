b31fcaa519268d96d373ede2fa0a518fd7b566dc
tatacastro12 <91699554+tatacastro12@users.noreply.github.com>
2023-02-08 13:51:28 -0800

    cambio de Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,21 +1,15 @@
 import React, { useState } from "react";
-
 const Form = ({ addTodo }) => {
     const [inputValue, setInputValue] = useState("");
-
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
     };
-
     const handleFormSubmit = (e) => {
         e.preventDefault();
-
         if (inputValue.trim() === "") return;
-
-        addTodo ({ title: inputValue, completed: false });
+        addTodo({ title: inputValue, completed: false });
         setInputValue("");
     };
-
     return (
         <form className="ui form" onSubmit={handleFormSubmit}>
             <div className="ui grid center aligned">
@@ -25,17 +19,17 @@ const Form = ({ addTodo }) => {
                             value={inputValue}
                             onChange={handleInputChange}
                             type="text"
-                            placeholder="Type your schedule for to do"
+                            placeholder="Enter something to do..."
                         />
                     </div>
-
                     <div className="column one wide">
-                        <button type="submit" className="ui button circular icon green"><i className="white plus icon"></i></button>
+                        <button type="submit" className="ui button circular icon green"><i className="white plus icon"></i>
+                        </button>
                     </div>
                 </div>
             </div>
         </form>
     );
 };
-
 export default Form;
+1
