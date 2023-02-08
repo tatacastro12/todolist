3e9ac2acedf7181e9a7a56a6cb35ac9c92221d91
tatacastro12 <91699554+tatacastro12@users.noreply.github.com>
2023-02-08 13:39:48 -0800

    cambio de app

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,40 @@
-import React,{useState} from 'react'
-import Form  from "./components/Form";
+import React, { useState } from 'react'
+import Form from "./components/Form";
 import Section from "./components/Section";
 import List from './components/List';
 
 
 const Apptitle = "to-do-App";
 
-const list= [
-  {title: "test #1", completed: false},
-  {title: "test #2"},
-  {title: "test #3"}
+const list = [
+  { title: "test #1", completed: false },
+  { title: "test #2" },
+  { title: "test #3" }
 ];
 
 const App = () => {
   const [todoList, setTodoList] = useState(list);
+
+  const addTodo = (item) => {
+    setTodoList((oldlist) => [...oldlist, item]);
+  };
+
   return (
     <div className='ui container center aligned'>
-    <></>
+      <></>
       <Section>
         <h1>
           {Apptitle}
         </h1>
       </Section>
+
       <Section>
-         <Form  /> 
-        <Section />
-        <List list={todoList} />
+        <Form addTodo={addTodo} />
       </Section>
+
+      <Section>
+      <List list={todoList} />
+    </Section>
     </div>
   )
 }
