a0beb85334dd44c14409070cecc3c7b2d243efa0
tatacastro12 <91699554+tatacastro12@users.noreply.github.com>
2023-02-08 11:16:02 -0800

    test #1

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,47 +1,51 @@
-import React from "react";
-import react, { useState } from "react";
-
-const Todo = ({title}) => {
-    const [Value, setvalue] =useState(title)
+import React, { useState } from "react";
+const Todo = ({ title }) => {
     const [isEditing, setIsEditing] = useState(false);
-    const handledivdubleclick = () => {
+    const [Value, setValue] = useState(title)
+    const [tempValue, setTempValue] = useState(title);
+    const handleDivDubleClick = () => {
         setIsEditing(true);
     };
-
     const handleInputKeyDown = (e) => {
-        const key = e.keycode;
-        if (key === 13 || key === 27) {
+        const key = e.keyCode;
+        if (key === 13) {
+            setValue(tempValue);
+            setIsEditing(false);
+        } else if (key === 27) {
+            setTempValue(Value);
             setIsEditing(false);
         }
     };
-
+    const handleInputOnChange = (e) => {
+        setTempValue(e.target.value);
+    };
     return (
-        isEditing ?
-            <div className="row" onDoubleClick={handledivdubleclick}>
-                <div className="column seven wide">
-                    <div className="ui input fluid">
-                        <input onChange={handleInputOnChange}
-                        onKeyDown={handleInputKeyDown}
-                        autoFocus={true}
-                        value={Value}
-                        />
-                    </div>
-                </div>
-            </div> :
-            <div className="row" onDoubleClick={handledivdubleclick}>
-                <div className="column five wide">
-                    <h1>{Value}</h1>
-                </div>
-                <div className="column two wide">
-                    <button className="ui button circular icon green"><i className="white check icon"></i>
-                    </button>
-                </div>
-                <div className="column two wide">
-                    <button className="ui button circular icon red"><i className="white remove icon"></i>
-                    </button>
-                </div>
-            </div>
+        <div className="row" onDoubleClick={handleDivDubleClick}>
+            {
+                isEditing ?
+                    <div className="column seven wide">
+                        <div className="ui input fluid">
+                            <input
+                                onChange={handleInputOnChange}
+                                onKeyDown={handleInputKeyDown}
+                                autoFocus={true}
+                                value={tempValue}
+                            />
+                        </div>
+                    </div> :
+                    <>
+                        <div className="column five wide">
+                            <h1>{Value}</h1>
+                        </div>
+                        <div className="column two wide">
+                            <button className="ui button circular icon green"><i className="white check icon"></i></button>
+                        </div>
+                        <div className="column two wide">
+                            <button className="ui button circular icon red"><i className="white remove icon"></i></button>
+                        </div>
+                    </>
+            }
+        </div>
     );
 };
-
 export default Todo;
