ff1eea0e045dcbcbabd80ce888459f1315370e36
tatacastro12 <91699554+tatacastro12@users.noreply.github.com>
2023-02-09 13:51:40 -0800

    backend

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 mongoose.set('strictQuery', true)
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGODB_URI, connectionOptions )
   .then(() => console.log("Connected successfully"))
   .catch((err) => console.error(err));
 
